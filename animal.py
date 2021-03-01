a64d132972051111b773a01e938c6a189693c6ee
Patryck Harley <patryck@outlook.com>
2021-03-01 19:15:32 -0300

    Delete animal.py

--- animal.py
+++
@@ -1,4 +0,0 @@
-animal = input('Qual animal você gostaria de ser? ')
-print(f'\n - Num primeiro momento, eu gostaria de ser um {animal}\n')
-animal2 = input(f'Qual animal você gostaria de ser no lugar do {animal}? ')
-print(f'\n - Pensando melhor, eu gostaria mesmo de ser um {animal2}.')
